470662bf6e11e2be5d567fff5b38cbaf97c93add
matthew-coudert-cko <113023884+matthew-coudert-cko@users.noreply.github.com>
2024-02-23 19:00:29 +0000

    Revert "Add support for removing Access Requests"

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/mutate/AddAccessRequestResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/mutate/AddAccessRequestResolver.java
@@ -1,16 +1,20 @@
 package com.linkedin.datahub.graphql.resolvers.mutate;
 
 import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.bindArgument;
+import static com.linkedin.datahub.graphql.resolvers.mutate.MutationUtils.buildMetadataChangeProposalWithUrn;
+import static com.linkedin.metadata.Constants.ACCESS_REQUESTS_ASPECT_NAME;
 
+import com.linkedin.common.*;
 import com.linkedin.common.urn.Urn;
+import com.linkedin.data.template.StringMap;
 import com.linkedin.datahub.graphql.QueryContext;
 import com.linkedin.datahub.graphql.generated.AddAccessRequestInput;
 import com.linkedin.datahub.graphql.generated.StringMapEntryInput;
-import com.linkedin.datahub.graphql.resolvers.mutate.util.AccessRequestUtils;
 import com.linkedin.metadata.entity.EntityService;
+import com.linkedin.metadata.entity.EntityUtils;
+import com.linkedin.mxe.MetadataChangeProposal;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
-
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,7 +38,21 @@ public class AddAccessRequestResolver implements DataFetcher<CompletableFuture<B
             log.info("Adding AccessRequest. input: {}", input);
             QueryContext context = environment.getContext();
             Urn actor = Urn.createFromString(context.getAuthentication().getActor().toUrnStr());
-            AccessRequestUtils.addAccessRequestToEntity(actor, additionalMetadataInput, targetUrn, _entityService);
+            AuditStamp created = EntityUtils.getAuditStamp(actor);
+
+            com.linkedin.common.AccessRequest accessRequest = new AccessRequest();
+            accessRequest.setCreated(created);
+
+            StringMap additionalMetadata = new StringMap();
+
+            for (StringMapEntryInput element : additionalMetadataInput) {
+              additionalMetadata.put(element.getKey(), element.getValue());
+            }
+            accessRequest.setAdditionalMetadata(additionalMetadata);
+            MetadataChangeProposal proposal =
+                generateAddAccessRequestEvent(targetUrn, accessRequest, _entityService);
+            log.info("Sending proposal {}", proposal);
+            _entityService.ingestProposal(proposal, created, false);
             return true;
           } catch (Exception e) {
             log.error(
@@ -45,4 +63,25 @@ public class AddAccessRequestResolver implements DataFetcher<CompletableFuture<B
         });
   }
 
+  public MetadataChangeProposal generateAddAccessRequestEvent(
+      Urn targetUrn, AccessRequest accessRequest, EntityService entityService) {
+    com.linkedin.common.AccessRequests currentAccessRequests =
+        (com.linkedin.common.AccessRequests)
+            EntityUtils.getAspectFromEntity(
+                targetUrn.toString(),
+                ACCESS_REQUESTS_ASPECT_NAME,
+                _entityService,
+                new AccessRequests());
+
+    AccessRequestArray accessRequestArray = new AccessRequestArray();
+
+    if (currentAccessRequests.hasRequests()) {
+      accessRequestArray = currentAccessRequests.getRequests();
+    }
+    accessRequestArray.add(accessRequest);
+    currentAccessRequests.setRequests(accessRequestArray);
+
+    return buildMetadataChangeProposalWithUrn(
+        targetUrn, ACCESS_REQUESTS_ASPECT_NAME, currentAccessRequests);
+  }
 }
